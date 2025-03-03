d6d49d28bd093326c6601fdaa1e3f81c4f73a5b4
pistorio3 <joao.pistorio@gmail.com>
2025-03-03 15:30:23 -0300

    add codebase

---
+++ src/main/java/br/com/pistorio/RestWithSpringBootAndJavaPistorioApplication.java
@@ -0,0 +1,13 @@
+package br.com.pistorio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithSpringBootAndJavaPistorioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestWithSpringBootAndJavaPistorioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/pistorio/RestWithSpringBootAndJavaPistorioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pistorio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringBootAndJavaPistorioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
