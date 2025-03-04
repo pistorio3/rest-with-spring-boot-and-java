910d38de7b7d523228dd312492688759d92b3af2
pistorio3 <joao.pistorio@gmail.com>
2025-03-03 23:22:02 -0300

    add mocked crud methods

--- src/main/java/br/com/pistorio/PersonServices.java
+++ src/main/java/br/com/pistorio/PersonServices.java
@@ -3,6 +3,8 @@ package br.com.pistorio;
 import br.com.pistorio.model.Person;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -13,6 +15,19 @@ public class PersonServices {
 
     private final Logger logger = Logger.getLogger(PersonServices.class.getName());
 
+    public List<Person> findAll() {
+        logger.info("Finding all Person!");
+
+        List<Person> persons = new ArrayList<>();
+
+        for (int i = 1; i <= 10 ; i++) {
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+
+        return persons;
+    }
+
     public Person findById(String id) {
         logger.info("Finding one Person!");
 
@@ -22,6 +37,34 @@ public class PersonServices {
         person.setLastName("Pistorio");
         person.setAddress("123 Main St");
         person.setGender("Male");
+
+        return person;
+    }
+
+    public Person create(Person person) {
+        logger.info("Create one Person!");
+
+        return person;
+    }
+
+    public Person update(Person person) {
+        logger.info("Updating one Person!");
+
+        return person;
+    }
+
+    public void delete(String id) {
+        logger.info("Deleting one Person!");
+    }
+
+    private Person mockPerson(int i) {
+        Person person = new Person();
+
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Pessoa " + i);
+        person.setLastName("Sobrenome");
+        person.setAddress("Endereço");
+        person.setGender("Gênero");
 
         return person;
     }
--- src/main/java/br/com/pistorio/controllers/PersonController.java
+++ src/main/java/br/com/pistorio/controllers/PersonController.java
@@ -4,20 +4,55 @@ import br.com.pistorio.PersonServices;
 import br.com.pistorio.model.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/person")
 public class PersonController {
 
     @Autowired
-    private PersonServices personServices;
+    private PersonServices personService;
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = "/{id}",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
     public Person findById(@PathVariable("id") String id) {
-        return personServices.findById(id);
+        return personService.findById(id);
+    }
+
+    @RequestMapping(value = "/all",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public List<Person> findAll() {
+        return personService.findAll();
+    }
+
+    @RequestMapping(value = "/create",
+            method = RequestMethod.POST,
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE
+    )
+    public Person create(@RequestBody Person person) {
+        return personService.create(person);
+    }
+
+    @RequestMapping(value = "/update",
+            method = RequestMethod.PUT,
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE
+    )
+    public Person update(@RequestBody Person person) {
+        return personService.update(person);
+    }
+
+    @RequestMapping(value = "/delete/{id}",
+            method = RequestMethod.DELETE
+    )
+    public void delete(@PathVariable("id") String id) {
+        personService.delete(id);
     }
 }
