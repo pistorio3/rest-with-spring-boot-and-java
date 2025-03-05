358867cb59fbd91e3fa8314f76f204ea4ac9b0aa
pistorio3 <joao.pistorio@gmail.com>
2025-03-05 09:58:56 -0300

    refactor logs

--- src/main/java/br/com/pistorio/services/PersonServices.java
+++ src/main/java/br/com/pistorio/services/PersonServices.java
@@ -3,17 +3,18 @@ package br.com.pistorio.services;
 import br.com.pistorio.exception.ResourceNotFoundException;
 import br.com.pistorio.model.Person;
 import br.com.pistorio.repository.PersonRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
-import java.util.logging.Logger;
 
 @Service
 public class PersonServices {
     private final AtomicLong counter = new AtomicLong();
-    private final Logger logger = Logger.getLogger(PersonServices.class.getName());
+    private final Logger logger = LoggerFactory.getLogger(PersonServices.class.getName());
 
     @Autowired
     PersonRepository personRepository;
